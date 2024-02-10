40e858363a8ad21b4a5b27d73702b44a1eb9b2cd
Fadjar Aidil <m.fadjar@uni.net.id>
2024-02-10 12:16:39 +0700

    updated: app

--- app/Config/App.php
+++ app/Config/App.php
@@ -1,2 +1,14 @@
 <?php
-##### COMING SOON #####
+
+namespace App\Config;
+
+class App{
+    public static function AppName()
+    {
+        return getenv('APP_NAME');
+    }
+    public static function AppUrl()
+    {
+        return getenv('APP_URL');
+    }
+}
